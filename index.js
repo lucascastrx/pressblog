be4a94684d2e788e9e91d87e8616ab41d7debae9
Lucas Castro <luukascastro@gmail.com>
2021-09-13 10:52:43 -0300

    Home page and client article page

--- index.js
+++ index.js
@@ -25,7 +25,28 @@ app.use('/', categoriesController)
 app.use('/', articlesController)
 
 app.get('/', (req, res) => {
-  res.render('index')
+  Article.findAll().then(articles => {
+    res.render('index', { articles: articles })
+  })
+})
+
+app.get('/:slug', (req, res) => {
+  const slug = req.params.slug
+  Article.findBy({
+    where: {
+      slug: slug
+    }
+  })
+    .then(article => {
+      if (article != undefined) {
+        res.render('article', { article: article })
+      } else {
+        res.redirect('/')
+      }
+    })
+    .catch(e => {
+      res.redirect('/')
+    })
 })
 
 app.listen(8080, () => {
