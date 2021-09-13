e02c02dc54764c65b93ed99e51107fd4953cf781
Lucas Castro <luukascastro@gmail.com>
2021-09-13 08:53:55 -0300

    TinyMCE editor enabled for text area whereas created articles

--- database/database.js
+++ database/database.js
@@ -1,7 +1,8 @@
 const sequelize = require('sequelize')
 const conn = new sequelize('pressblog', 'root', 'admin', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  timezone: '-03:00'
 })
 
 module.exports = conn
