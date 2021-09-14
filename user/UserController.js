9ec27ab84fe8759441aa25321daf8350f0e42a52
Lucas Castro <luukascastro@gmail.com>
2021-09-14 15:05:13 -0300

    Hashed password

--- user/UserController.js
+++ user/UserController.js
@@ -1,13 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const User = require('./User')
+const bcrypt = require('bcryptjs')
 
 router.get('/admin/users', (req, res) => {
-  res.send('OK!')
+  User.findAll().then(users => {
+    res.render('admin/users/index', {
+      users: users
+    })
+  })
 })
 
 router.get('/admin/users/create', (req, res) => {
   res.render('admin/users/create')
+})
+
+router.post('/users/create', (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  User.findOne({
+    where: {
+      email: email
+    }
+  }).then(user => {
+    if (user == undefined) {
+      const salt = bcrypt.genSaltSync(10)
+      const hash = bcrypt.hashSync(password, salt)
+
+      User.create({
+        email: email,
+        password: hash
+      })
+        .then(() => {
+          res.redirect('/')
+        })
+        .catch(e => {
+          res.redirect('/')
+        })
+    } else {
+      res.redirect('/admin/users/create')
+    }
+  })
 })
 
 module.exports = router
